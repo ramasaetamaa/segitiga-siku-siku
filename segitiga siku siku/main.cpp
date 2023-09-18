5c119ae7b1ca18ec6a2e9930eedcc5aee692aeb9
Asus ROG Strix G15 <Asus ROG Strix G15@LAPTOP-HA1LS1TS>
2023-09-18 20:58:59 +0700

    Add project files.

---
+++ segitiga siku siku/main.cpp
@@ -0,0 +1,24 @@
+// mencari sisi miring segitiga siku siku
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	float a, t, c;
+	cout << "\tProgram Menghitung Sisi Miring Segitiga Siku-Siku" << endl;
+	cout << "\t-------------------------------------------------" << endl;
+	cout << "Masukan alas segitiga : ";
+	cin >> a;
+	cout << "Masukan tinggi segitiga : ";
+	cin >> t;
+	c = sqrt((a * a) + (t * t));
+	cout << "------------------------" << endl;
+	cout << " Hasilnya adalah : " << c << endl;
+
+	return 0;
+
+}
+
